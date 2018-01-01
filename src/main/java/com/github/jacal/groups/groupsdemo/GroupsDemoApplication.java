00721fffd94d22982d7137ef909caa6f721c03b3
DESKTOP-EORT59H\Alex <alexandre.jacquot@gmail.com>
2018-01-01 14:43:42 +0100

    Project initialization with spring Initializr (https://start.spring.io/)

---
+++ src/main/java/com/github/jacal/groups/groupsdemo/GroupsDemoApplication.java
@@ -0,0 +1,12 @@
+package com.github.jacal.groups.groupsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupsDemoApplication.class, args);
+	}
+}
